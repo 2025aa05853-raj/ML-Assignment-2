f4d037d67707093c60576c1c0a3e3423a7fcbd91
2025aa05853-raj <2025aa05853@wilp.bits-pilani.ac.in>
2026-02-14 02:27:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from sklearn.preprocessing import StandardScaler
 data = load_breast_cancer()
 record_count = len(data.data)
 target_count = len(data.target)
+feature_count = len(data.feature_names)
+
 print(len(data.data))
 st.text("Record Count : " + str(record_count))
 st.text("Target Count : " + str(target_count))
+st.text("Feature Count : " + str(feature_count))
