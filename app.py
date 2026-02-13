d2283e1e734a15c821de07c81b6a6dbd13ae29c1
2025aa05853-raj <2025aa05853@wilp.bits-pilani.ac.in>
2026-02-14 02:24:10 +0530

    Loading dataset directly from sklearn and checking data count from Streamlit

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
-st.text("Hello...My first application in Streamlit")
-print("Hello Raj")
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+data = load_breast_cancer()
+record_count = len(data.data)
+target_count = len(data.target)
+print(len(data.data))
+st.text("Record Count : " + record_count)
+st.text("Target Count : " + target_count)
